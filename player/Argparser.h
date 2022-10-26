2632e2a1cc23a4762f75eda1f9881d986701bb31
AZAZZZEL <shifer.k0d@gmail.com>
2022-10-26 16:16:59 +0700

    adding nothing

--- player/Argparser.h
+++ player/Argparser.h
@@ -5,16 +5,20 @@
 #include "music.h"
 
 
-template <class Type>
+template <typename Type>
 std::istream& operator>>(std::istream& i, const Type& arg) { return i; }
 
-template <class Type>
+template <typename Type>
 void argpars(Type& arg, std::string& str)
 {
 	std::stringstream buff;
 	buff << str;
 	buff >> arg;
+	std::string buff2;
+	buff2 = str;
 	str.clear();
 	std::getline(buff, str);
+	if (str == buff2) //Nothing changed
+		throw std::exception("Not valid argument");
 }
 
